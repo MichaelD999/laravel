15f582340b4782ff5d1f5727e740d16a91d42f08
Michael Diaz <michaeldiaz@Michaels-MacBook-Pro.local>
2021-03-17 14:38:37 +0100

    Form handling

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 class ArticlesController extends Controller
 {
     //
+    public function index()
+    {
+        $articles = Article::latest()->get();
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+
+
+
     public function show($id)
     {
         $article = Article::find($id);
@@ -17,11 +26,18 @@ class ArticlesController extends Controller
 
     public function create()
     {
-
+        return view('articles.create');
     }
 
     public function store()
     {
+        $article = new Article();
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles');
 
     }
 
